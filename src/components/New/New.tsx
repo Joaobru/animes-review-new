70d9432b7a2378ca50ba46133982978768a120a9
joaobru <joaopaulobruno123@gmail.com>
2023-02-16 00:03:07 -0300

    feat: create a component to use in mf

---
+++ src/components/New/New.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * @export
+ * @component
+ * @name New
+ *
+ * @description
+ * Componente de exemplo para exportar do mf.
+ */
+
+export const New = () => {
+  return (
+    <h2>New MF</h2>
+  )
+};
